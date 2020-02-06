aba85aac7de83ff172ef1abf3f520ea9118875d0
StonebayKyle <stonebay.kyle@gmail.com>
2020-02-06 11:26:56 -0700

    Completes a5 exercise 3

--- src/Picture.java
+++ src/Picture.java
@@ -95,6 +95,17 @@ public class Picture extends SimplePicture {
 		}
 	}
 
+	/** Method to keep only blue */
+	public void keepOnlyBlue() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setRed(0);
+				pixelObj.setGreen(0);
+			}
+		}
+	}
+
 	/**
 	 * Method that mirrors the picture around a vertical mirror in the center of
 	 * the picture from left to right
