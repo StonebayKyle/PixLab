fa6a550035497b61d48f07b28af6a9e3883f37b1
StonebayKyle <stonebay.kyle@gmail.com>
2020-02-19 08:50:58 -0700

    Completes a6 exercise 1

--- src/Picture.java
+++ src/Picture.java
@@ -160,6 +160,20 @@ public class Picture extends SimplePicture {
 		}
 	}
 
+	public void mirrorVerticalRightToLeft() {
+		Pixel[][] pixels = this.getPixels2D();
+		Pixel leftPixel = null;
+		Pixel rightPixel = null;
+		int width = pixels[0].length;
+		for (int row = 0; row < pixels.length; row++) {
+			for (int col = width / 2; col < width; col++) {
+				leftPixel = pixels[row][width - 1 - col];
+				rightPixel = pixels[row][col];
+				leftPixel.setColor(rightPixel.getColor());
+			}
+		}
+	}
+
 	/** Mirror just part of a picture of a temple */
 	public void mirrorTemple() {
 		int mirrorPoint = 276;
